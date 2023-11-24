1af2c112221768048893d9adbbad368b472fb03e
Tim108 <tim.sonderen@nedap.com>
2023-11-24 14:11:08 +0100

    day 4

---
+++ day4/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"strconv"
+)
+
+var input = "ckczppom"
+
+func main() {
+	for i := 0; i < 1000000; i++ {
+		str := input + strconv.Itoa(i)
+		hash := createHash(str)
+
+		if hash[:5] == "00000" {
+			println("The answer is", strconv.Itoa(i), "with hash", hash)
+			return
+		}
+
+	}
+	println("Found nothing :(")
+}
+
+func createHash(key string) (hash string) {
+	data := []byte(key)
+	checksum := md5.Sum(data)
+	hash = hex.EncodeToString(checksum[:])
+	return
+}
