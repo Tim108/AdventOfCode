727377248c52af9c6bb444dbd41445d011bb80f5
Tim108 <tim.sonderen@nedap.com>
2023-11-24 16:15:03 +0100

    day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -8,37 +8,85 @@ import (
 
 //go:embed input.txt
 var input string
-var vowels = "aeiou"
-var bannedSequences = []string{"ab", "cd", "pq", "xy"}
 
 func main() {
 	niceWordcount := 0
 	for _, word := range strings.Split(input, "\n") {
-		if niceWord(word) {
+		if property4(word) && property5(word) {
 			niceWordcount++
 		}
 	}
 	println("Nice words:", niceWordcount)
 }
 
-func niceWord(word string) (result bool) {
+// It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+func property1(word string) (result bool) {
+	vowels := "aeiou"
 	vowelCount := 0
-	repeatedChar := false
-
-	for i, char := range word {
+	for _, char := range word {
 		if strings.Contains(vowels, string(char)) {
 			vowelCount++
 		}
+	}
+	return vowelCount >= 3
+}
+
+// It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+func property2(word string) (result bool) {
+	for i, char := range word {
 		if i < len(word)-1 {
 			nextChar := rune(word[i+1])
 			if char == nextChar {
-				repeatedChar = true
+				return true
 			}
+		}
+	}
+	return false
+}
+
+// It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+func property3(word string) (result bool) {
+	bannedSequences := []string{"ab", "cd", "pq", "xy"}
+	for i, char := range word {
+		if i < len(word)-1 {
+			nextChar := rune(word[i+1])
 			sequence := string(char) + string(nextChar)
 			if slices.Contains(bannedSequences, sequence) {
 				return false
 			}
 		}
 	}
-	return vowelCount >= 3 && repeatedChar
+	return true
+}
+
+// It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+func property4(word string) (result bool) {
+	pairs := []string{}
+
+	for i, char := range word {
+		if i <= len(word)-2 {
+			nextChar := rune(word[i+1])
+			pair := string(char) + string(nextChar)
+
+			if len(pairs) > 0 && slices.Contains(pairs[:len(pairs)-1], pair) {
+				return true
+			} else {
+				pairs = append(pairs, pair)
+			}
+		}
+	}
+	return false
+}
+
+// It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+func property5(word string) (result bool) {
+	for i, char := range word {
+		if i <= len(word)-3 {
+			nextCharOver := rune(word[i+2])
+			if char == nextCharOver {
+				return true
+			}
+		}
+	}
+	return false
 }
